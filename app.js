e05621261a81697f51945a381765f1f3f0d5ffa6
Carlos N Reina <cnreina@gmail.com>
2022-01-12 18:47:56 -0700

    cors

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 
 const path = require('path');
 
-// express
 const express = require('express');
 const bodyParser = require('body-parser');
-// controller
+
 const errorController = require('./controllers/errorController');
+
+const cors = require('cors')
+const corsOptions = {
+  origin: "https://cse341nodejsapp.herokuapp.com/",
+  optionsSuccessStatus: 200
+};
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://cnreina:mPass_3762@cluster0.gcajw.mongodb.net/shop?retryWrites=true&w=majority";
+
 // app
 const app = express();
+app.use(cors(corsOptions));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
 // routes
 const homeRoutes = require('./routes/homeRoute');
 app.use(bodyParser.urlencoded({ extended: false }));
+
 // static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(homeRoutes);
