50a78202ff9a4c71989ec53238dbdedb6011d364
Carlos N Reina <cnreina@gmail.com>
2022-01-14 13:50:09 -0700

    setup

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-const path = require('path');
-
+// includes
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
+// routes
+const homeRoutes = require('./routes/homeRoute');
 const errorController = require('./controllers/errorController');
 
 // app
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
-// routes
-const homeRoutes = require('./routes/homeRoute');
 app.use(bodyParser.urlencoded({ extended: false }));
-
 // static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(homeRoutes);
