03625c0831da082811fa5d698c4b99ad9668a4d0
Carlos N Reina <cnreina@gmail.com>
2022-01-13 23:50:59 -0700

    hide connection string

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ const errorController = require('./controllers/errorController');
 
 // app
 const app = express();
-
-// app.use(cors(corsOptions));
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -23,11 +20,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(homeRoutes);
 app.use(errorController.get404);
 
-// start server
-mongoose.connect('mongodb+srv://cnreina:mPass_3762@cluster0.gcajw.mongodb.net/shop?retryWrites=true&w=majority')
-.then(result => {
-  app.listen(3000);
-})
-.catch(err => {
-  console.log(err);
+// get mongoDB connection string
+const fs = require('fs');
+const filePath = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'mongodbstring.txt'
+);
+
+fs.readFile(filePath, (err, fileContent) => {
+  if (err) {
+    console.log(err);
+  } else {
+    // start server
+    mongoose.connect(fileContent.toString())
+    .then(result => {
+      app.listen(3000);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
 });
