0f499a16602ea6ebb08d228af0a5208553c6d07e
Carlos N Reina <cnreina@gmail.com>
2022-01-14 15:38:42 -0700

    docs

--- app.js
+++ app.js
@@ -43,9 +43,16 @@ fs.readFile(filePath, (err, fileContent) => {
       useNewUrlParser: true,
       family: 4
     };
+
+    // Heroku's architecture requires the use of config vars.
+    // Express calls .listen(PORT), which makes use of the Heroku config var
+    // Using || to initialize PORT's value to the first defined variable.
+    // When app is run on Heroku, process.env.PORT is defined and passed to .listen().
+    // Running locally, the config var is undefined and the localhost port is passed to .listen().
     const MONGODB_URL = process.env.MONGODB_URL || fileContent.toString();
     const PORT = process.env.PORT || 3000;
 
+    // start server
     mongoose.connect(MONGODB_URL, options)
     .then(result => {
       app.listen(PORT);
@@ -53,6 +60,5 @@ fs.readFile(filePath, (err, fileContent) => {
     .catch(err => {
       console.log(err);
     });
-
   }
 });
